694ab6a7269a5fd74a2db090a92d54a0be65fca0
samuelcsng <samuelcsng@gmail.com>
2024-10-07 11:43:04 +0800

    20241007.1143

--- demo-sb-restapi/src/main/java/com/bootcamp/demo/demo_sb_restapi/model/dto/jph/UserDTO.java
+++ demo-sb-restapi/src/main/java/com/bootcamp/demo/demo_sb_restapi/model/dto/jph/UserDTO.java
@@ -1,33 +1,49 @@
 package com.bootcamp.demo.demo_sb_restapi.model.dto.jph;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
+@Builder
 public class UserDTO {
   private int id;
   private String name;
   private String username;
-  // private String email;
+  private String email;
   private Address address;
   private String phone;
   private String website;
   private Company company;
+
+  @AllArgsConstructor
+  @NoArgsConstructor
   @Getter
+  @Builder
   public static class Address {
     private String street;
     private String suite;
     private String city;
     private String zipcode;
     private GEO geo;
+    @AllArgsConstructor
+    @NoArgsConstructor
     @Getter
+    @Builder
     public static class GEO {
       private String lat;
       private String lng;
     }
   }
   
+  @AllArgsConstructor
+  @NoArgsConstructor
   @Getter
-  private class Company {
+  @Builder
+  private static class Company {
     private String name;
     private String catchPhrase;
     private String bs;
--- demo-sb-restapi/src/main/java/com/bootcamp/demo/demo_sb_restapi/service/impl/JPHServiceImpl.java
+++ demo-sb-restapi/src/main/java/com/bootcamp/demo/demo_sb_restapi/service/impl/JPHServiceImpl.java
@@ -1,6 +1,9 @@
 package com.bootcamp.demo.demo_sb_restapi.service.impl;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import com.bootcamp.demo.demo_sb_restapi.model.dto.jph.UserDTO;
@@ -11,9 +14,18 @@ public class JPHServiceImpl implements JPHService {
 
   @Override
   public List<UserDTO> getUsers() {
-    UserDTO[] users = new RestTemplate()
-        .getForObject("https://jsonplaceholder.typicode.com/users", UserDTO[].class);
-  return List.of(users); // to list
+    UserDTO[] users = new RestTemplate().getForObject(
+        "https://jsonplaceholder.typicode.com/users", UserDTO[].class);
+    // return List.of(users); // to list
+    // return List.of(users).stream() //
+    // .collect(Collectors.toList()) //
+    // ;
+    // return Stream.of(users) //
+    // .collect(Collectors.toList()) //
+    // ;
+    return Arrays.stream(users) //
+        .collect(Collectors.toList()) //
+    ;
   }
 
 }
